fc07f83e498d9a2262cb7f079cfa99a59f8d1809
Eri Cleiton França da Silva <ecfs1988@hotmail.com>
2021-10-12 15:10:05 -0300

    Add project files.

---
+++ Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoexception : Exception
+    {
+
+        public JogoNaoCadastradoexception()
+            : base("Este jogo já está cadastrado")
+        { }
+    }
+}
